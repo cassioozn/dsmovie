7b7d1230a13ebec363cf2ba9ebc3c9731175b430
Cassio <cassio.coelho@cilia.com.br>
2022-01-14 15:52:09 -0300

    Netlify correction on == to ===

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -33,11 +33,11 @@ function getFills(score: number) {
 }
 
 function Star({fill} : StarProps){
-    if(fill == 0){
+    if(fill === 0){
         return <StarEmpty/>
     }
     else{
-        if (fill == 1){
+        if (fill === 1){
             return <StarFull/>
         }
         else{
